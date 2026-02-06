ad5784199979b7a8660b12e06dba4efc469499d5
aylioness <aylioness@gmail.com>
2026-02-06 15:08:44 -0500

    Updates Lab.1 message

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Aylin" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hello! My name is Aylin Godfrey and I am a junior from West Virginia. One fun fact about me is that I lived in Juneau, Alaska for four years when I was very little. I play the saxophone, read a lot, write a little bit, and participate in a lot of sports, including cross country, track, mountain biking, and mixed martial arts.")
 
 if __name__ == "__main__":
     main()
